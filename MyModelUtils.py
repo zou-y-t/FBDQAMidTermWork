a135eaea7291a0a6ede18716d4fa9630f909575e
Alan <115681059+zou-y-t@users.noreply.github.com>
2024-10-12 09:02:54 +0800

    Create MyModelUtils.py
    initial the ModelUtils file

---
+++ MyModelUtils.py
@@ -0,0 +1,158 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+class Model1:
+    '''
+    Model1: y=alpha+beta*x
+    '''
+    def __init__(self, r8, r1):
+        self.r8 = np.array(r8)
+        self.r1 = np.array(r1)
+        self.beta = 0
+        self.alpha = 0
+        self.predictR8 = np.array([])
+
+    def fit(self):
+        # 计算斜率beta
+        self.beta = np.cov(self.r1, self.r8)[0][1] / np.var(self.r1)
+        # 计算截距alpha
+        self.alpha = np.mean(self.r8) - self.beta * np.mean(self.r1)
+        # 计算预测值
+        self.predictR8 = self.alpha + self.beta * self.r1
+        # 计算R2
+        self.R2 = 1-(self.r8 - self.predictR8).dot(self.r8 - self.predictR8) / np.sum((self.r8 - np.mean(self.r8)) ** 2)
+
+    def getBeta(self):
+        return self.beta
+
+    def getAlpha(self):
+        return self.alpha
+
+    def getR2(self):
+        return self.R2
+
+    def predict(self, r):
+        return self.alpha + self.beta * r
+
+    def plot(self):
+        plt.scatter(self.r1, self.r8)
+        plt.plot(self.r1, self.alpha + self.beta * self.r1, color='red')
+        plt.xlabel('r1')
+        plt.ylabel('r8')
+        plt.title('Scatter plot with regression line')
+        plt.show()
+
+class Model2:
+    '''
+    Model2: y=alpha+beta1*x1+beta2*x2
+    '''
+    def __init__(self,r8,r1,r7):
+        self.r8=np.array(r8)
+        self.r1=np.array(r1)
+        self.r7=np.array(r7)
+        self.beta_r1=0
+        self.beta_r7=0
+        self.alpha=0
+        self.predictR8=np.array([])
+        self.R2=0
+
+    def fit(self):
+        # 计算斜率beta
+        self.beta_r1=np.cov(self.r1,self.r8)[0][1]/np.var(self.r1)
+        self.beta_r7=np.cov(self.r7,self.r8)[0][1]/np.var(self.r7)
+        # 计算截距alpha
+        self.alpha=np.mean(self.r8)-self.beta_r1*np.mean(self.r1)-self.beta_r7*np.mean(self.r7)
+        # 计算预测值
+        self.predictR8=self.alpha+self.beta_r1*self.r1+self.beta_r7*self.r7
+        # 计算R2
+        self.R2 = 1-(self.r8 - self.predictR8).dot(self.r8 - self.predictR8) / np.sum((self.r8 - np.mean(self.r8)) ** 2)
+
+    def getBeta_r1(self):
+        return self.beta_r1
+    
+    def getBeta_r7(self):
+        return self.beta_r7
+    
+    def getAlpha(self):
+        return self.alpha
+    
+    def getR2(self):
+        return self.R2
+    
+    def predict(self,r1,r7):
+        return self.alpha+self.beta_r1*r1+self.beta_r7*r7
+    
+    def plot(self):
+        fig=plt.figure()
+        ax=fig.add_subplot(111,projection='3d')
+        ax.scatter(self.r1,self.r7,self.r8)
+        x1=np.linspace(min(self.r1),max(self.r1),100)
+        x2=np.linspace(min(self.r7),max(self.r7),100)
+        x1,x2=np.meshgrid(x1,x2)
+        y=self.alpha+self.beta_r1*x1+self.beta_r7*x2
+        ax.plot_surface(x1,x2,y,cmap='rainbow')
+        ax.set_xlabel('r1')
+        ax.set_ylabel('r7')
+        ax.set_zlabel('r8')
+        plt.show()
+    
+class DataCleaner:
+    '''
+    DataCleaner: df是2022/4/1-2022/6/30的分钟数据，这个类取了半小时的数据，计算了r
+    '''
+    half_hour_time=[
+        # 半小时时间
+        "10:00:00",
+        "10:30:00",
+        "11:00:00",
+        "11:30:00",
+        "13:30:00",
+        "14:00:00",
+        "14:30:00",
+        "15:00:00"
+    ]
+    def __init__(self, df):
+        self.df=df
+        self.r1, self.r2, self.r3, self.r4, self.r5, self.r6, self.r7, self.r8 = [], [], [], [], [], [], [], []
+        self.r = [self.r1, self.r2, self.r3, self.r4, self.r5, self.r6, self.r7, self.r8]
+
+
+    def clean(self):
+        # 去除半小时结点的数据
+        self.df['date']=pd.to_datetime(self.df['date'])
+        self.df = self.df[self.df['date'].dt.time.astype(str).isin(self.half_hour_time)].copy()
+        # 计算r
+        self.df.loc[:,'r']=self.df['close']/self.df['close'].shift(1)-1
+        # 去除2022/4/1的数据（缺损了第一个数据点r1）
+        self.df=self.df[self.df['date'].dt.date.astype(str)!="2022-04-01"] 
+        #将r依次存入r1,r2,r3,r4,r5,r6,r7,r8
+        for i in range(len(self.df)):
+            self.r[i % 8].append(self.df['r'].iloc[i])
+
+    def getR(self):
+        return self.r
+    
+    def getR1(self):
+        return self.r1
+    
+    def getR2(self):
+        return self.r2
+    
+    def getR3(self):
+        return self.r3
+    
+    def getR4(self):
+        return self.r4
+    
+    def getR5(self):
+        return self.r5
+    
+    def getR6(self):
+        return self.r6
+    
+    def getR7(self):
+        return self.r7
+    
+    def getR8(self):
+        return self.r8
